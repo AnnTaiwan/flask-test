34669f32b9f4443ce22f3afad725677674b1f893
Ann <ann20021007@gmail.com>
2024-08-12 15:27:19 +0100

    update

--- module/app.py
+++ module/app.py
@@ -3,17 +3,6 @@ This code is used to activate a local server, and then I will use Postman to act
 '''
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import os
-import matplotlib
-import torch
-from observe_audio_function_ver3 import load_audio, SAMPLE_RATE
-# Set Matplotlib backend if needed, Agg is not interactive backend.
-# To prevent warning from "Starting a Matplotlib GUI outside of the main thread will likely fail."
-matplotlib.use('Agg')  # Set the backend to 'Agg' before importing pyplot
-
-AUDIO_FOLDER_NAME = "data/audio_files"
-IMAGE_FOLDER_NAME = "data/image_files"
-PTH_PATH = "app/model_9_ENG_ver1.pth"
 
 app = Flask(__name__)
 CORS(app)
@@ -24,38 +13,21 @@ def home():
 
 @app.route('/test', methods=['GET'])
 def get_test():
-    return '8/11 Hello world!!'
+    return 'Hello world!! This is test page.'
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    if not os.path.exists(AUDIO_FOLDER_NAME):
-        os.makedirs(AUDIO_FOLDER_NAME)
-    if not os.path.exists(IMAGE_FOLDER_NAME):
-        os.makedirs(IMAGE_FOLDER_NAME)
-
-    # print(f"Clean the files in {IMAGE_FOLDER_NAME}.")
-    for i in os.listdir(IMAGE_FOLDER_NAME):
-        i_path = os.path.join(IMAGE_FOLDER_NAME, i)
-        os.remove(i_path)
-
+@app.route('/add', methods=['POST'])
+def add_two_numbers():
+    # Extract data from the JSON request
+    data = request.get_json()
+    a = data.get("a")
+    b = data.get("b")
     
-    
-    # Check if any file was uploaded
-    if not any(request.files.values()):
-        return jsonify({'error': 'No files uploaded'}), 400
+    # Perform the addition
+    if a is not None and b is not None:
+        ans = a + b
+        return jsonify({"result": ans})
+    else:
+        return jsonify({"error": "Invalid input"}), 400
 
-    # Currently, each audio changed into one image, and do one prediction
-    for file_key in request.files:
-        # dealt with updated files, with the correct key typed in Postman
-        audio = request.files[file_key]
-
-        # Save the uploaded file to a temporary location
-        audio_path = os.path.join(AUDIO_FOLDER_NAME, audio.filename)
-        audio.save(audio_path)
-        audio1 , sr = load_audio(audio_path, sr=SAMPLE_RATE)
-        
-        # directly remove the audio
-        os.remove(audio_path)
-    
-    return jsonify({"audio":len(audio1),
-                    "sr":sr})
+if __name__ == '__main__':
+    app.run(debug=False)
