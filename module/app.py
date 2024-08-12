4f49e2de9a56abe6c7c6eb5cf09418cd2fd13340
Ann <ann20021007@gmail.com>
2024-08-12 14:45:52 +0100

    First commit

---
+++ module/app.py
@@ -0,0 +1,16 @@
+'''
+This code is used to activate a local server, and then I will use Postman to act as front end.
+'''
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return 'Flask API started on Pythonanywhere. Hi there!'
+
+@app.route('/test', methods=['GET'])
+def get_test():
+    return '8/11 Hello world!!'
